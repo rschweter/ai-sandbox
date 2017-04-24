6634146c33532f1cd23c77a70ba31807e73461c5
Ryan Schweter <ryan@teamchaos.com>
2017-04-23 22:31:28 -0500

    Initial commit

---
+++ Assets/Scripts/Brain.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+using AForge.Fuzzy;
+
+public class Brain : MonoBehaviour {
+	public List<FuzzySetGroupCharts> Charts;
+	private InferenceSystem _inferenceSystem;
+	private Rigidbody _rb;
+
+	protected void Start() {
+		_rb = GetComponent<Rigidbody>();
+		InitFuzz();
+	}
+
+	protected void Update() {
+		float right = GetWallDistance(transform.right);
+		float left = GetWallDistance(-transform.right);
+		float front = GetWallDistance(transform.forward);
+
+		_inferenceSystem.SetInput("RightDistance", right);
+		_inferenceSystem.SetInput("LeftDistance", left);
+		_inferenceSystem.SetInput("FrontDistance", front);
+
+		// get the fuzzy angle and rotate towards it
+		try {
+			float angle = _inferenceSystem.Evaluate("Heading");
+			Quaternion delta = Quaternion.Euler(0, angle, 0);
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, transform.rotation * delta, 180 * Time.deltaTime);
+		} catch (Exception) {
+			// logging to troubleshoot holes in the fuzzy ruleset
+			Debug.LogError(String.Format("Failed to get heading for L:{0} F:{1} R:{2}", left, front, right));
+		}
+		
+		// force velocity
+		_rb.velocity = transform.forward * 2;
+	}
+
+	private void InitFuzz() {
+		this.Charts = new List<FuzzySetGroupCharts>();
+		Database db = new Database();
+
+		// fuzzy set to determine perceived distance
+		FuzzySetGroupDesc distanceGroup = new FuzzySetGroupDesc("Distance");
+		distanceGroup.Add(new FuzzySetDescLeft("Near", 0.75f, 2.5f));
+		distanceGroup.Add(new FuzzySetDescMid("Medium", 0.75f, 2.5f, 3, 10));
+		distanceGroup.Add(new FuzzySetDescRight("Far", 3, 10));
+
+		this.Charts.Add(new FuzzySetGroupCharts(distanceGroup));
+		FuzzySet[] distanceSets = FuzzUtil.CreateFuzzySets(distanceGroup);
+
+		db.AddVariable(FuzzUtil.CreateVariable("RightDistance", 0, 10, distanceSets));
+		db.AddVariable(FuzzUtil.CreateVariable("LeftDistance", 0, 10, distanceSets));
+		db.AddVariable(FuzzUtil.CreateVariable("FrontDistance", 0, 10, distanceSets));
+
+		// fuzzy set to determine heading directions
+		FuzzySetGroupDesc headingGroup = new FuzzySetGroupDesc("Heading");
+		headingGroup.Add(new FuzzySetDescLeft("HardLeft", -35, -30));
+		headingGroup.Add(new FuzzySetDescMid("Left", -35, -30, -20, -15));
+		headingGroup.Add(new FuzzySetDescMid("SlightLeft", -20, -15, -5, 0));
+		headingGroup.Add(new FuzzySetDescMid("Straight", -5, -1, 1, 5));
+		headingGroup.Add(new FuzzySetDescMid("SlightRight", 0, 5, 15, 20));
+		headingGroup.Add(new FuzzySetDescMid("Right", 15, 20, 30, 35));
+		headingGroup.Add(new FuzzySetDescRight("HardRight", 30, 35));
+
+		this.Charts.Add(new FuzzySetGroupCharts(headingGroup));
+		FuzzySet[] headingSets = FuzzUtil.CreateFuzzySets(headingGroup);
+
+		db.AddVariable(FuzzUtil.CreateVariable("Heading", -50, 50, headingSets));
+
+		_inferenceSystem = new InferenceSystem(db, new CentroidDefuzzifier(50));
+		
+		// if all is clear ahead go for it
+		_inferenceSystem.NewRule("1", "IF FrontDistance IS Far THEN Heading IS Straight");
+
+		// should handle left/right turns
+		_inferenceSystem.NewRule("2", "IF FrontDistance IS Near AND RightDistance IS Near THEN Heading IS Left");
+		_inferenceSystem.NewRule("3", "IF FrontDistance IS Near AND LeftDistance IS Near THEN Heading IS Right");
+
+		// edge away from walls
+		_inferenceSystem.NewRule("4", "IF LeftDistance IS Near AND (RightDistance IS Medium OR RightDistance IS Far) THEN Heading IS SlightRight");
+		_inferenceSystem.NewRule("5", "IF RightDistance IS Near AND (LeftDistance IS Medium OR LeftDistance IS Far) THEN Heading IS SlightLeft");
+
+		// attempt to give priority to left turns if both are available?
+		_inferenceSystem.NewRule("6", "IF FrontDistance IS Near THEN Heading IS Left");
+	}
+
+	private float GetWallDistance(Vector3 direction) {
+		RaycastHit[] hits = Physics.RaycastAll(transform.position, direction, float.MaxValue, ~LayerMask.NameToLayer("Obstacle"), QueryTriggerInteraction.UseGlobal);
+		if (hits.Length == 0) return float.MaxValue;
+
+		// linq bad, but so good
+		RaycastHit closest = hits.OrderBy(x => x.distance).FirstOrDefault();
+		return closest.distance;
+	}
+
+	// public void OnDrawGizmos() {
+	// 	float distance = GetWallDistance(transform.forward);
+	// 	Gizmos.DrawLine(transform.position, transform.position + transform.forward * distance);
+	// }
+}
---
+++ Assets/Scripts/FuzzUtil.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+using AForge;
+using AForge.Fuzzy;
+
+public class FuzzUtil  {
+	public static LinguisticVariable CreateVariable(string name, float start, float end, params FuzzySet[] labels) {
+		LinguisticVariable lv = new LinguisticVariable(name, start, end);
+		
+		for (int i = 0; i < labels.Length; i++) {
+			lv.AddLabel(labels[i]);
+		}
+
+		return lv;
+	}
+
+	public static FuzzySet[] CreateFuzzySets(FuzzySetGroupDesc group) {
+		FuzzySet[] sets = new FuzzySet[group.Sets.Count];
+
+		for (int i = 0; i < group.Sets.Count; i++) {
+			FuzzySetDesc desc = group.Sets[i];
+			sets[i] = new FuzzySet(desc.Name, new PiecewiseLinearFunction(desc.Points));
+		}
+
+		return sets;
+	}
+}
+
+[Serializable]
+public class FuzzySetGroupDesc {
+	public string Name;
+	public List<FuzzySetDesc> Sets;
+	public float ActualMin;
+	public float ActualMax;
+
+	public FuzzySetGroupDesc(string name) {
+		this.Name = name;
+		this.Sets = new List<FuzzySetDesc>();
+		this.ActualMin = float.MaxValue;
+		this.ActualMax = float.MinValue;
+	}
+
+	// currently requires the points to be pre-sorted
+	public FuzzySetDesc Add(FuzzySetDesc desc) {
+		this.Sets.Add(desc);
+
+		float min = desc.Points[0].X;
+		float max = desc.Points[desc.Points.Length - 1].X;
+
+		if (min < this.ActualMin) {
+			this.ActualMin = min;
+		}
+
+		if (max > this.ActualMax) {
+			this.ActualMax = max;
+		}
+
+		return desc;
+	}
+
+	public float GetChartMin() {
+		return this.ActualMin - (this.ActualMax - this.ActualMin) * 0.1f;
+	}
+
+	public float GetChartMax() {
+		return this.ActualMax + (this.ActualMax - this.ActualMin) * 0.1f;
+	}
+}
+
+[Serializable]
+public abstract class FuzzySetDesc {
+	public string Name;
+	public Point[] Points;
+
+	public FuzzySetDesc(string name) {
+		this.Name = name;
+	}
+}
+
+public class FuzzySetDescLeft : FuzzySetDesc {
+	public FuzzySetDescLeft(string name, float start, float end) 
+		: base(name) {
+
+		this.Points = new Point[] { 
+			new Point(start, 1), 
+			new Point(end, 0) 
+		};
+	}
+}
+
+public class FuzzySetDescMid : FuzzySetDesc {
+	public FuzzySetDescMid(string name, float start, float peakLeft, float peakRight, float end) 
+		: base(name) {
+
+		this.Points = new Point[] { 
+			new Point(start, 0),
+			new Point(peakLeft, 1),
+			new Point(peakRight, 1),
+			new Point(end, 0)
+		};
+	}
+}
+
+public class FuzzySetDescRight : FuzzySetDesc {
+	public FuzzySetDescRight(string name, float start, float end) 
+		: base(name) {
+
+		this.Points = new Point[] { 
+			new Point(start, 0), 
+			new Point(end, 1) 
+		};
+	}
+}
+
+[Serializable]
+public class FuzzySetGroupCharts {
+	public string Name;
+	public List<FuzzySetChart> Charts;
+
+	public FuzzySetGroupCharts(FuzzySetGroupDesc group) {
+		this.Name = group.Name;
+
+		float chartMin = group.GetChartMin();
+		float chartMax = group.GetChartMax();
+
+		this.Charts = new List<FuzzySetChart>();
+		for (int i = 0; i < group.Sets.Count; i++) {
+			this.Charts.Add(new FuzzySetChart(group.Sets[i], chartMin, chartMax));
+		}
+	}
+}
+
+[Serializable]
+public class FuzzySetChart {
+	public string Name;
+	public AnimationCurve Curve;
+
+	public FuzzySetChart(FuzzySetDesc set, float chartMin, float chartMax) {
+		this.Name = set.Name;
+
+		foreach (Point p in set.Points) {
+			Debug.Log(set.Name + " " + p.X + " " + p.Y);
+		}
+
+		List<Keyframe> frames = new List<Keyframe>(set.Points.Length + 2);
+		Keyframe? last = null;
+
+		if (!Mathf.Approximately(set.Points[0].X, chartMin)) {
+			last = new Keyframe(chartMin, set.Points[0].Y, 0, 0);
+			frames.Add(last.Value);
+		}
+
+		for (int i = 0; i < set.Points.Length; i++) {
+			Point p = set.Points[i];
+
+			float outTangent = 0;
+			if (last.HasValue) {
+				outTangent = (p.Y - last.Value.value) / (p.X - last.Value.time);
+				Keyframe kf = last.Value;
+				kf.outTangent = outTangent;
+				last = kf;
+				frames[frames.Count - 1] = kf;
+			}
+
+			Keyframe next = new Keyframe(p.X, p.Y, outTangent, 0);
+			frames.Add(next);
+			last = next;
+		}
+
+		if (last.HasValue && !Mathf.Approximately(last.Value.time, 1f)) {
+			Keyframe next = new Keyframe(chartMax, last.Value.value, 0, 0);
+			frames.Add(next);
+		}
+
+		this.Curve = new AnimationCurve(frames.ToArray());
+	}
+}
